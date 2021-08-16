84e04cf349da8a27e5e169aa5483e2b1a631e66d
Felix De Haro <felixddhs@outlook.com>
2021-08-16 16:32:49 -0500

    Added code for the button

--- app/src/main/java/mx/tec/a2nddegree/MainActivity.kt
+++ app/src/main/java/mx/tec/a2nddegree/MainActivity.kt
@@ -2,10 +2,19 @@ package mx.tec.a2nddegree
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Code for the Calculation button
+
+        val buttonCalculate : Button =findViewById(R.id.calculateButton)
+        buttonCalculate.setOnClickListener(){
+
+        }
     }
 }
